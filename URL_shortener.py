51a64727d0fe261ecb1a815a6b8b4bdf6fd9c2cc
Kirill Bobyrev <kbobyrev@hotmail.com>
2014-11-23 17:36:10 +0300

    some minor improvements to code
    shout-out to PEP

--- URL_shortener.py
+++ URL_shortener.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, redirect, request
-from storage import get_entries, add_entry
+from storage import get_entries, add_entry, get_answer
 
 app = Flask(__name__)
 
@@ -14,20 +14,27 @@ add_entry({
 print(get_entries())
 '''
 
+
 @app.route('/')
 def get_home():
-    return render_template('home.html', input_field_state = 'neutral', entries = [])
+    return render_template('home.html', input_field_state='neutral', entries=get_entries())
 
+
+# request some path probably stored before
 @app.route('/<path>')
 def go_to_path(path):
-    #TODO: check if path is valid or not and then handle it
+    if get_answer(path):
+        return redirect(get_answer(path)), 302
     return render_template('404.html')
 
-@app.route("/create_long_url", methods=["POST"])
-def new_long_url():
-    longURL = request.form['input']
-    print(longURL)
+
+# try to create new
+@app.route("/create_short_url", methods=["POST"])
+def new_short_url():
+    longurl = request.form['input']
+    print(longurl)
     return redirect('/')
+
 
 if __name__ == '__main__':
     app.run()
--- storage.py
+++ storage.py
@@ -2,19 +2,32 @@ import os
 
 FILENAME = os.path.dirname(os.path.realpath(__file__)) + '\\storage.txt'
 
+
 def load_entries_from_storage_():
     with open(FILENAME, 'r', encoding='utf-8') as storage:
         return eval(storage.read())
+
 
 def update_storage_():
     with open(FILENAME, 'w') as storage:
         print(repr(entries_), file=storage)
 
+
+# Entry = {'short_url' : '', 'long_url' : '', ip : ''}
 entries_ = load_entries_from_storage_()
+
 
 def get_entries():
     return entries_
 
+
 def add_entry(entry):
     entries_.append(entry)
     update_storage_()
+
+
+def get_answer(short_url):
+    for entry in entries_:
+        if short_url == entry['short_url']:
+            return entry['long_url']
+    return False
