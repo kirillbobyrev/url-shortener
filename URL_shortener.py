ae8195adfa4646c57930e39957095b7fa973335d
Kirill Bobyrev <kbobyrev@hotmail.com>
2014-11-24 19:50:18 +0300

    Fixed bugs, deployed to server

--- URL_shortener.py
+++ URL_shortener.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, redirect, request, json, current_app, url_for
-from storage import get_entries, add_entry, get_answer
+from flask import Flask, render_template, redirect, request, json, current_app
+from storage import add_entry, get_answer
 import re
 import random
 
@@ -21,7 +21,6 @@ def go_to_path(path):
     if get_answer(path):
         return redirect(get_answer(path)), 302
     return render_template('404.html')
-
 
 @app.route("/create_short_url", methods=["POST"])
 def create_short_url():
@@ -48,6 +47,9 @@ def create_short_url():
                                         'short_specify': 'on' in short_specify, 'HOST_URL': request.url_root}
 
     if valid_url(long_url) and not get_answer(short_url):
+        if 'http://' not in long_url and 'https://' not in long_url:
+            long_url = 'http://' + long_url
+
         cookie['status'] = 'success'
         add_entry({
             'short_url': short_url,
@@ -62,11 +64,17 @@ def create_short_url():
     return response
 
 def valid_url(url):
-    if url == '' or url == []:
-        return False
-    if 'http:/' not in url and 'https:/' not in url:
-        url = 'http:/' + url
-    return re.match(r'^[a-zA-Z]+://', url)
+    if 'http://' not in url and 'https://' not in url:
+        url = 'http://' + url
+    regex = re.compile(
+        r'^(?:http|ftp)s?://'  # http:// or https://
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
+        r'localhost|' # localhost...
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|'  # ...or ipv4
+        r'\[?[A-F0-9]*:[A-F0-9:]+\]?)'  # ...or ipv6
+        r'(?::\d+)?'  # optional port
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+    return re.match(regex, url)
 
 if __name__ == '__main__':
     app.debug = True
